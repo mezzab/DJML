13eb3c86f730ddcaa38e4ba4aa818c8381541c30
diegoalfonsin <diegoalfonsin@hotmail.com>
2015-11-06 21:38:12 -0300

    migracion pasaje con errores + cambios pk en clientes clie_id

--- src/AerolineaFrba/Abm Ruta/FormRutaListado.cs
+++ src/AerolineaFrba/Abm Ruta/FormRutaListado.cs
@@ -21,24 +21,27 @@ namespace AerolineaFrba.Abm_Ruta
 
         private void FormRutaListado_Load(object sender, EventArgs e)
         {
-            //SqlConnection conexion = new SqlConnection();
-            //conexion.ConnectionString = Settings.Default.CadenaDeConexion;
+            cargarGrid();
+         
+        }
 
-            string qry = "SELECT DISTINCT RUTA_CODIGO as Codigo, " +
-                                         "(SELECT CIUD_DETALLE FROM DJML.CIUDADES WHERE CIUD_ID = RUTA_CIUDAD_ORIGEN) as Origen, " +
-                                         "(SELECT CIUD_DETALLE FROM DJML.CIUDADES WHERE CIUD_ID = RUTA_CIUDAD_DESTINO) as Destino, " +
-                                         "(SELECT SERV_DESCRIPCION FROM DJML.SERVICIOS WHERE SERV_ID = RUTA_SERVICIO_ID) as Servicio, " +
-                                         "'$ ' + cast ([RUTA_PRECIO_BASE_PASAJE] as CHAR(100)) as Pasaje, " +
-                                         "'$ ' + cast ([RUTA_PRECIO_BASE_KILO] as CHAR(100)) as 'Kilo Encomienda', " +      
-                         "FROM DJML.RUTAS" +
-                         "ORDER BY 2";
-
-            dataGrid.DataSource = new Query(qry).ObtenerDataTable();
+        private void cargarGrid() 
+        {
+            SqlConnection conexion = new SqlConnection();
+            conexion.ConnectionString = Settings.Default.CadenaDeConexion;
+    
+            Query qry2 = new Query("select ruta_codigo from djml.rutas ");
+            datos.DataSource = qry2.ObtenerDataTable();
         }
 
         private void button_volver_Click(object sender, EventArgs e)
         {
             //ToDo
+        }
+
+        private void dataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
 
     }
