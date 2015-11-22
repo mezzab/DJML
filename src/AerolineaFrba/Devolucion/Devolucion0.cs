e193aeada061b273d419fbf27b7df99086ddf0e2
Marcos Mezzabotta <marcos32m@gmail.com>
2015-11-22 18:30:47 -0300

    Devolucion, Check OK

--- src/AerolineaFrba/Devolucion/Devolucion0.cs
+++ src/AerolineaFrba/Devolucion/Devolucion0.cs
@@ -17,6 +17,10 @@ namespace AerolineaFrba.Devolucion
         public static string id_compra;
         public static bool yaBuscoP = false;
         public static bool yaBuscoE = false;
+
+        public static List<int> IDsEncomiendas = new List<int>();
+        public static List<int> IDsPasajes = new List<int>();
+
         public Devolucion0()
         {
             InitializeComponent();
@@ -55,6 +59,9 @@ namespace AerolineaFrba.Devolucion
 
         private void Agregar_Click(object sender, EventArgs e)
         {
+            IDsEncomiendas.Clear();
+            IDsPasajes.Clear();
+
             if (existeCompra())
               {
 
@@ -75,14 +82,16 @@ namespace AerolineaFrba.Devolucion
              
                     //pasajes1.Columns["ID"].Visible = false;
 
-                    if(yaBuscoP == false) 
+                    /*if(yaBuscoP == false) 
                     {
                         DataGridViewCheckBoxColumn chk1 = new DataGridViewCheckBoxColumn();
                         chk1.HeaderText = "Seleccionar";
                         chk1.Name = "CheckBox";
                         pasajes1.Columns.Add(chk1);
                         yaBuscoP = true;
-                    }
+                        chk1.FalseValue = "0";
+                        chk1.TrueValue = "1";
+                    }*/
 
                     //pasajes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
                     DataGridViewColumn column = pasajes1.Columns[0];
@@ -139,14 +148,16 @@ namespace AerolineaFrba.Devolucion
              
 
 
-                    if (yaBuscoE == false)
+                    /*if (yaBuscoE == false)
                     {
                         DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
                         chk.HeaderText = "Seleccionar";
                         chk.Name = "CheckBox";
                         encomiendas.Columns.Add(chk);
                         yaBuscoE = true;
-                    }
+                        chk.FalseValue = "0";
+                        chk.TrueValue = "1";
+                    }*/
                     DataGridViewColumn column41 = encomiendas.Columns[7];
                     column41.Width = 70;
                 
@@ -166,21 +177,147 @@ namespace AerolineaFrba.Devolucion
 
         private void pasajes1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            DataGridViewCheckBoxCell ch1 = new DataGridViewCheckBoxCell();
+            ch1 = (DataGridViewCheckBoxCell)pasajes1.Rows[pasajes1.CurrentRow.Index].Cells[0];
 
+            if (ch1.Value == null)
+                ch1.Value = false;
+            switch (ch1.Value.ToString())
+            {
+                case "True":
+                    ch1.Value = false;
+                    break;
+                case "False":
+                    ch1.Value = true;
+                    break;
+            }
+
+            if (ch1.Value.ToString() == "True")
+            {
+                //MessageBox.Show("ID = " + pasajes1.Rows[e.RowIndex].Cells[1].Value.ToString() + " Y CHECK CON TIC");
+
+                int idSeleccionado = Convert.ToInt32(pasajes1.Rows[e.RowIndex].Cells[1].Value);
+
+                if (estaEnLaLista(IDsPasajes, idSeleccionado) == false)
+                {
+                    IDsPasajes.Add(idSeleccionado);
+                }
+                if (estaEnLaLista(IDsPasajes, idSeleccionado) == true)
+                {
+                    //MessageBox.Show("ESTO NUNCA DEBERIA OCURRIR");
+                }
+                var message = string.Join(Environment.NewLine, IDsPasajes);
+                MessageBox.Show("La lista de pasajes a dar de baja ahora es: " + message);
+            }
+
+            if (ch1.Value.ToString() == "False")
+            {
+               // MessageBox.Show("ID = " + pasajes1.Rows[e.RowIndex].Cells[1].Value.ToString() + " Y CHECK SIN TIC");
+
+                int idSeleccionado = Convert.ToInt32(pasajes1.Rows[e.RowIndex].Cells[1].Value);
+
+                if (estaEnLaLista(IDsPasajes, idSeleccionado) == true)
+                {
+                    borrarDeLista(IDsPasajes, idSeleccionado);
+                }
+                if (estaEnLaLista(IDsPasajes, idSeleccionado) == false)
+                {
+                  //  MessageBox.Show("ESTO NUNCA DEBERIA OCURRIR");
+                }
+                
+                var message = string.Join(Environment.NewLine, IDsPasajes);
+                MessageBox.Show("La lista de pasajes a dar de baja ahora es: " + message);
+            }
         }
 
         private void encomiendas_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            DataGridViewCheckBoxCell ch = new DataGridViewCheckBoxCell();
+            ch = (DataGridViewCheckBoxCell)encomiendas.Rows[encomiendas.CurrentRow.Index].Cells[0];
 
+            if (ch.Value == null)
+                ch.Value = false;
+            switch (ch.Value.ToString())
+            {
+                case "True":
+                    ch.Value = false;
+                    break;
+                case "False":
+                    ch.Value = true;
+                    break;
+            }
+
+            if (ch.Value.ToString() == "True")
+            {
+                //MessageBox.Show("ID = " + encomiendas.Rows[e.RowIndex].Cells[1].Value.ToString() + " CHECK CON TIC");
+
+                int idSeleccionado = Convert.ToInt32(encomiendas.Rows[e.RowIndex].Cells[1].Value);
+
+                if (estaEnLaLista(IDsEncomiendas,idSeleccionado) == false)
+                {
+                    IDsEncomiendas.Add(idSeleccionado);
+                }
+                if (estaEnLaLista(IDsEncomiendas,idSeleccionado) == true)
+                {
+                 //  MessageBox.Show("ESTO NUNCA DEBERIA OCURRIR");
+                }
+
+                var message = string.Join(Environment.NewLine, IDsEncomiendas);
+                MessageBox.Show("La lista de encomiendas a dar de baja ahora es: " + message);
+            }
+
+            if (ch.Value.ToString() == "False")
+            {
+                //MessageBox.Show("ID = " + encomiendas.Rows[e.RowIndex].Cells[1].Value.ToString() + " CHECK SIN TIC");
+           
+                int idSeleccionado = Convert.ToInt32(encomiendas.Rows[e.RowIndex].Cells[1].Value);
+
+                if (estaEnLaLista(IDsEncomiendas,idSeleccionado) == true)
+                {
+                    borrarDeLista(IDsEncomiendas, idSeleccionado);
+                }
+                 if (estaEnLaLista(IDsEncomiendas,idSeleccionado) == false)
+                {
+                  // MessageBox.Show("ESTO NUNCA DEBERIA OCURRIR");
+                }
+                 var message = string.Join(Environment.NewLine, IDsEncomiendas);
+                 MessageBox.Show("La lista de encomiendas a dar de baja ahora es: " + message);
+            }
+
+        }
+
+        private bool estaEnLaLista(List<int> lista, int id)
+        { 
+            bool esta = false;
+            for (int i = 0; i <= lista.Count - 1; i++)
+            {
+                if (lista[i] == id)
+                {
+                    esta = true;
+                }
+            }
+            return esta;
+        }
+
+        private bool borrarDeLista(List<int> lista, int id)
+        {
+            bool esta = false;
+            for (int i = 0; i <= lista.Count - 1; i++)
+            {
+                if (lista[i] == id)
+                {
+                    lista.RemoveAt(i);
+                }
+            }
+            return esta;
         }
 
         private void encomiendas_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-           
+           /*
             if (e.ColumnIndex > 0)
             {
 
-              MessageBox.Show("ENTRO 1");
                 if (encomiendas.Rows[e.RowIndex].Cells[7].Value != null)
                 {
                     MessageBox.Show("ENTRO 2");
@@ -198,11 +335,11 @@ namespace AerolineaFrba.Devolucion
                 {
                     MessageBox.Show("ID = " + encomiendas.Rows[e.RowIndex].Cells[0].Value.ToString() + " CHECK CON TIC");
                 }
-            }
+            }*/
         }
 
         private void pasajes1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
+        {/*
             if (e.ColumnIndex > 0)
             {
                 if (pasajes1.Rows[e.RowIndex].Cells[8].Value != null)
@@ -222,7 +359,7 @@ namespace AerolineaFrba.Devolucion
                     MessageBox.Show("ID = " + pasajes1.Rows[e.RowIndex].Cells[0].Value.ToString() + " CHECK CON TIC");
                 }
 
-            }
+            }*/
         }
     
     }
