4955e2441b32ccf4cd436f429ad908db5a99fc18
diegoalfonsin <diegoalfonsin@hotmail.com>
2015-11-24 03:00:17 -0300

    registro llegada destino

--- src/AerolineaFrba/Registro Llegada Destino/FormRegistroLlegada.cs
+++ src/AerolineaFrba/Registro Llegada Destino/FormRegistroLlegada.cs
@@ -38,14 +38,14 @@ namespace AerolineaFrba.Registro_Llegada_Destino
 
 
             Query qry7 = new Query("select v.viaje_id  from djml.viajes v "
-            +" join djml.rutas r on v.viaje_ruta_id = r.ruta_codigo"
-            +" join djml.tramos t on r.ruta_tramo = t.tramo_id "
-            +" join djml.ciudades c1 on t.tramo_ciudad_origen = c1.ciud_id"
-            +" join djml.ciudades c2 on t.tramo_ciudad_destino = c2.ciud_id"
-            +" where v.viaje_aero_id = '" + comboBoxAeronaves.Text + "' "
-            +" and c1.ciud_detalle = (select CIUD_ID from djml.CIUDADES WHERE CIUD_DETALLE ='" + comboBoxCiudadOrigen.Text + "') " 
-            +" and c2.ciud_detalle = (select CIUD_ID from djml.CIUDADES WHERE CIUD_DETALLE ='" + comboBoxCiudadDestino.Text + "') " 
-            +" and v.viaje_id not in (select rd_viaje_id from djml.registro_destino");
+            + " join djml.rutas r on v.viaje_ruta_id = r.ruta_codigo"
+            + " join djml.tramos t on r.ruta_tramo = t.tramo_id "
+            + " join djml.ciudades c1 on t.tramo_ciudad_origen = c1.ciud_id"
+            + " join djml.ciudades c2 on t.tramo_ciudad_destino = c2.ciud_id"
+            + " where v.viaje_aero_id = '" + comboBoxAeronaves.Text + "' "
+            + " and c1.ciud_id = (select CIUD_ID from djml.CIUDADES WHERE CIUD_DETALLE ='" + comboBoxCiudadOrigen.Text + "')"
+            + " and c2.ciud_id = (select CIUD_ID from djml.CIUDADES WHERE CIUD_DETALLE ='" + comboBoxCiudadDestino.Text + "')"
+            + " and v.viaje_id not in (select rd_viaje_id from djml.registro_destino)");
 
             datos.DataSource = qry7.ObtenerDataTable();
 
@@ -136,26 +136,7 @@ namespace AerolineaFrba.Registro_Llegada_Destino
         
         }
 
-        private void bnGuardar_Click(object sender, EventArgs e)
-        {
-            if (comboBoxAeronaves.SelectedIndex != -1 && comboBoxCiudadOrigen.SelectedIndex != -1 && comboBoxCiudadDestino.SelectedIndex != -1)
-            {
-                //HACER EL INSERT DE LA TABLA REGISTRO DESTINO
 
-                /*
-
-
-                MessageBox.Show("Registro destino Generado Correctamente", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            
-                 */
-                  
-                  
-            }
-            else
-            {
-                MessageBox.Show("Complete todos los campos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
 
         private void comboBoxAeronaves_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -175,7 +156,41 @@ namespace AerolineaFrba.Registro_Llegada_Destino
 
         private void datos_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            if (comboBoxAeronaves.SelectedIndex != -1 && comboBoxCiudadOrigen.SelectedIndex != -1 && comboBoxCiudadDestino.SelectedIndex != -1)
+            {
 
+                //HACER EL INSERT EN REGISTRO DESTINO
+                int viajeId;
+                viajeId = Convert.ToInt32(datos.Rows[e.RowIndex].Cells[1].Value.ToString());
+
+                //obtener datos de ciudad Origen
+                int ciudadOrigen;
+                Query qry3 = new Query("SELECT CIUD_ID FROM DJML.CIUDADES WHERE CIUD_DETALLE = '" + comboBoxCiudadOrigen.Text + "'");
+                ciudadOrigen = (int)qry3.ObtenerUnicoCampo();
+
+                //obtener datos de ciudad Destino
+                int ciudadDestino;
+                Query qry4 = new Query("SELECT CIUD_ID FROM DJML.CIUDADES WHERE CIUD_DETALLE = '" + comboBoxCiudadOrigen.Text + "'");
+                ciudadDestino = (int)qry4.ObtenerUnicoCampo();
+               
+
+
+               //hace la insercion en la base de datos del nuevo registro generado
+                string sql1 = "INSERT INTO DJML.REGISTRO_DESTINO(RD_VIAJE_ID, RD_AERO_ID, RD_FECHA_LLEGADA, RD_CIUDAD_ORIGEN_ID, RD_CIUDAD_DESTINO_ID)"
+                                + "values (" + viajeId +" ,'" + comboBoxAeronaves.SelectedValue.ToString() + "', "+ciudadOrigen+", "+ciudadDestino+" )";
+                Query qry = new Query(sql1);
+                qry.pComando = sql1;
+                qry.Ejecutar();
+                
+
+
+
+                MessageBox.Show("Registro destino Generado Correctamente", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Complete todos los campos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
 
